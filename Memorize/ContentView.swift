0e9cc399dde12666933170cc15f97b7f15a6811e
HuangSenhui <huangsenhui@live.com>
2020-07-05 16:16:28 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by HuangSenhui on 2020/7/5.
+//  Copyright © 2020 HuangSenhui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
